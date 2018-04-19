c5f6178e7e43a8a7f792366dfc3f58ab998c653f
Jim Bennett <jim.bennett@mac.com>
2018-04-19 16:48:06 +0100

    Added TensorFlow mobile post

---
+++ UsingTensorFlowAndAzureInAndroid/CustomVision/ImageClassifier.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Android.App;
+using Android.Graphics;
+using Org.Tensorflow.Contrib.Android;
+
+namespace CustomVision
+{
+    public class ImageClassifier
+    {
+        readonly List<string> labels;
+        readonly TensorFlowInferenceInterface inferenceInterface;
+
+        public ImageClassifier()
+        {
+            var assets = Application.Context.Assets;
+			inferenceInterface = new TensorFlowInferenceInterface(assets, "model.pb");
+
+            using (var sr = new StreamReader(assets.Open("labels.txt")))
+            {
+                var content = sr.ReadToEnd();
+                labels = content.Split('\n').Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).ToList();
+            }
+
+        }
+
+        static readonly int InputSize = 227;
+        static readonly string InputName = "Placeholder";
+        static readonly string OutputName = "loss";
+
+        public string RecognizeImage(Bitmap bitmap)
+        {
+            var outputNames = new[] { OutputName };
+            var floatValues = GetBitmapPixels(bitmap);
+            var outputs = new float[labels.Count];
+
+            inferenceInterface.Feed(InputName, floatValues, 1, InputSize, InputSize, 3);
+            inferenceInterface.Run(outputNames);
+            inferenceInterface.Fetch(OutputName, outputs);
+
+            var results = new List<Tuple<float, string>>();
+            for (var i = 0; i < outputs.Length; ++i)
+                results.Add(Tuple.Create(outputs[i], labels[i]));
+
+            return results.OrderByDescending(t => t.Item1).First().Item2;
+        }
+
+        static float[] GetBitmapPixels(Bitmap bitmap)
+        {
+            var floatValues = new float[227 * 227 * 3];
+
+            using (var scaledBitmap = Bitmap.CreateScaledBitmap(bitmap, 227, 227, false))
+            {
+                using (var resizedBitmap = scaledBitmap.Copy(Bitmap.Config.Argb8888, false))
+                {
+                    var intValues = new int[227 * 227];
+                    resizedBitmap.GetPixels(intValues, 0, resizedBitmap.Width, 0, 0, resizedBitmap.Width, resizedBitmap.Height);
+
+                    for (int i = 0; i < intValues.Length; ++i)
+                    {
+                        var val = intValues[i];
+
+                        floatValues[i * 3 + 0] = ((val & 0xFF) - 104);
+                        floatValues[i * 3 + 1] = (((val >> 8) & 0xFF) - 117);
+                        floatValues[i * 3 + 2] = (((val >> 16) & 0xFF) - 123);
+                    }
+
+                    resizedBitmap.Recycle();
+                }
+
+                scaledBitmap.Recycle();
+            }
+
+            return floatValues;
+        }
+    }
+}
---
+++ UsingTensorFlowAndAzureInAndroid/CustomVision/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.TextToSpeech;
+
+namespace CustomVision
+{
+	[Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/MyTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+		public Android.Support.V7.Widget.Toolbar Toolbar { get; set; }
+
+        Button takePhotoButton;
+		private Button TakePhotoButton => takePhotoButton ?? (takePhotoButton = FindViewById<Button>(Resource.Id.take_photo_button));
+
+        ImageView photoView;
+		private ImageView PhotoView => photoView ?? (photoView = FindViewById<ImageView>(Resource.Id.photo));
+
+        TextView resultLabel;
+		private TextView ResultLabel => resultLabel ?? (resultLabel = FindViewById<TextView>(Resource.Id.result_label));
+
+		ProgressBar progressBar;
+		private ProgressBar ProgressBar => progressBar ?? (progressBar = FindViewById<ProgressBar>(Resource.Id.progressbar));
+
+        readonly ImageClassifier imageClassifier = new ImageClassifier();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Main);
+
+			Toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+			if (Toolbar != null)
+				SetSupportActionBar(Toolbar);
+
+			SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+			SupportActionBar.SetHomeButtonEnabled(false);
+
+			TakePhotoButton.Click += TakePhotoButton_Click;
+		}
+
+		async void TakePhotoButton_Click(object sender, EventArgs e)
+		{
+			TakePhotoButton.Enabled = false;
+            ProgressBar.Visibility = ViewStates.Visible;
+
+            try
+            {
+                var image = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions { PhotoSize = PhotoSize.Medium });
+                var bitmap = await BitmapFactory.DecodeStreamAsync(image.GetStreamWithImageRotatedForExternalStorage());
+
+                PhotoView.SetImageBitmap(bitmap);
+                var result = await Task.Run(() => imageClassifier.RecognizeImage(bitmap));
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                CrossTextToSpeech.Current.Speak($"I think it is {result}");
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                ResultLabel.Text = result;
+            }
+            finally
+            {
+                TakePhotoButton.Enabled = true;
+                ProgressBar.Visibility = ViewStates.Invisible;
+            }
+		}
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults) => PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
+
